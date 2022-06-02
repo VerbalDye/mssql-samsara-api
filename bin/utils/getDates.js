942402ff98bac5d4c3f3ddb5730ab8aed909d7ee
Dustin Huntsberger <dhunts258@gmail.com>
2022-06-02 14:33:11 -0400

    fixed double negative issue

--- bin/utils/getDates.js
+++ bin/utils/getDates.js
@@ -1,7 +1,7 @@
 const dayjs = require("dayjs");
 
 const getDates = function (dayOffset, format) {
-    let end_time = dayjs().subtract(-dayOffset, 'day').set('hour', 4).set('minute', 0).set('second', 0);
+    let end_time = dayjs().subtract(dayOffset, 'day').set('hour', 4).set('minute', 0).set('second', 0);
     let start_time = end_time.subtract(1, 'day');
     if (format === 'rfc-full') {
         return { startTime: start_time.format('YYYY-MM-DDTHH:mm:ss[Z]'), endTime: end_time.format('YYYY-MM-DDTHH:mm:ss[Z]') };
