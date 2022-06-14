4f03d3c2a2fd2d304c9c8bf5a353023d18f6662e
Dustin Huntsberger <dhunts258@gmail.com>
2022-06-14 08:52:20 -0400

    added async

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 const sdk = require('api')('@samsara-dev-rel/v2019.01.01#d5k23zl3dr5d7e');
 sdk.auth(process.env.SAMSARA_AUTH);
 
-sequelize.sync({ force: false }).then(() => {
-    for(let x = 13; x < 469; x++) {
-    SamsaraDriverEfficiencyFunction(sdk, x);
-    SamsaraDriverHOSFunction(sdk, null, x);
-    SamsaraTractorOdometerFunction(sdk, x);
+sequelize.sync({ force: false }).then(async () => {
+    for (let x = 13; x < 469; x++) {
+        Promise.all(
+            SamsaraDriverEfficiencyFunction(sdk, x),
+            SamsaraDriverHOSFunction(sdk, null, x),
+            SamsaraTractorOdometerFunction(sdk, x)
+        )
     }
 });
